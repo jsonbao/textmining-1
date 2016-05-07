1181486020bdc87f60e4cc71dffddfc56214f796
Deepak <Deepak@Deepak-PC>
2016-05-07 14:08:49 +0100

    Added BarChart for Total number of words and sentences

--- src/com/deepak/textmining/controller/TextMiningController.java
+++ src/com/deepak/textmining/controller/TextMiningController.java
@@ -6,6 +6,12 @@ import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LegendPlacement;
+
 import com.deepak.textmining.util.MapUtils;
 import com.deepak.textmining.util.POSUtil;
 import com.deepak.textmining.util.StringUtils;
@@ -26,6 +32,8 @@ public class TextMiningController implements Serializable {
 
     private boolean showResult = false;
 
+    private BarChartModel barModel;
+
     public void clear() {
         showResult = false;
         stringToBeTested = "";
@@ -35,6 +43,7 @@ public class TextMiningController implements Serializable {
         //Checks if there are only spaces been given as input
         if (!getStringToBeTested().trim().isEmpty()) {
             showResult = true;
+            initBarModelForNumbeCount();
         }
     }
 
@@ -70,6 +79,41 @@ public class TextMiningController implements Serializable {
         return stringUtils.fetchWordsBasedOnSize(getStringToBeTested(), 4);
     }
 
+    public void initBarModelForNumbeCount() {
+        barModel = new BarChartModel();
+        barModel.setAnimate(true);
+        barModel.setBarWidth(350);
+        barModel.setTitle("Total Number of Sentences and Words");
+        barModel.setLegendPlacement(LegendPlacement.INSIDE);
+        barModel.setLegendPosition("ne");
+
+        ChartSeries sentences = new ChartSeries();
+        sentences.setLabel("Sentences");
+        sentences.set(barModel.getTitle(), getNumberOfSentences());
+
+        ChartSeries words = new ChartSeries();
+        words.setLabel("Words");
+        words.set(barModel.getTitle(), getTotalWordCount());
+
+        barModel.addSeries(sentences);
+        barModel.addSeries(words);
+
+        Axis xAxis = barModel.getAxis(AxisType.X);
+        xAxis.setLabel("Property");
+        Axis yAxis = barModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Total");
+        yAxis.setMin(0);
+        if (getTotalWordCount() <= Long.MAX_VALUE) {
+            long y = getTotalWordCount() + (100L - (getTotalWordCount() % 100));
+            yAxis.setMax((getTotalWordCount() % 100 < 100) ? y : getTotalWordCount());
+        } else {
+            yAxis.setMax(Long.MAX_VALUE);
+        }
+    }
+
+    /*
+     * Setters and Getters of Objects and Variables
+     */
     public StringUtils getStringUtils() {
         return stringUtils;
     }
@@ -84,6 +128,14 @@ public class TextMiningController implements Serializable {
 
     public String getStringToBeTested() {
         return stringToBeTested;
+    }
+
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+
+    public void setBarModel(BarChartModel barModel) {
+        this.barModel = barModel;
     }
 
     public boolean isShowResult() {
