7dee2b9041f8a04daeface6a35c315d8e22327f3
Deepak <Deepak@Deepak-PC>
2016-05-14 20:33:09 +0100

    Added Compare Text Analysis Controller, UI except results

---
+++ textmining-ui/src/com/textmining/ui/controller/CompareAnalysisController.java
@@ -0,0 +1,24 @@
+package com.textmining.ui.controller;
+
+public class CompareAnalysisController {
+
+    private String stringOne = "";
+
+    private String stringTwo = "";
+
+    public String getStringOne() {
+        return stringOne;
+    }
+
+    public String getStringTwo() {
+        return stringTwo;
+    }
+
+    public void setStringOne(String stringOne) {
+        this.stringOne = stringOne;
+    }
+
+    public void setStringTwo(String stringTwo) {
+        this.stringTwo = stringTwo;
+    }
+}
